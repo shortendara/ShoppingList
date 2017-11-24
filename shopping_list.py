6c16afc027f17d44960a2428aac6d1eb2ffb0889
Dara Shorten <dara.shorten@mycit.ie>
2017-11-24 13:29:44 -0800

    Reipes are being sent to email address

--- shopping_list.py
+++ shopping_list.py
@@ -4,15 +4,20 @@ from bs4 import element
 import urllib
 import re
 import recipe
+import smtplib
+import unicodedata
+from details import password, email_address
 
 #Function to retieve recipe
-def get_recipe(url):
+def get_recipe(recipe_url):
     #Variabales
     ingredients = []
     cooking_method =[]
 
     #Open URL using urllib
-    url = "https://www.bbc.co.uk/"+url
+    url = "https://www.bbc.co.uk/"+recipe_url
+    print url
+
     recipe_page = urllib.urlopen(url)
 
     #Use BeautifulSoup to get html text
@@ -20,17 +25,32 @@ def get_recipe(url):
 
     #Retrieve each list of recipe ingredients
     for ingredient in soup.find_all('ul', {'class': 'recipe-ingredients__list'}):
-        ingredients.append(ingredient.text)
+        #Convert text from unicode to python str
+        ingredients.append(ingredient.text.encode('utf-8'))
 
     #Retrieve method for cooking the dish
     for method in soup.find_all('p', {'class': 'recipe-method__list-item-text'}):
-        cooking_method.append(method.text)
+        #Convert text from unicode to python str
+        cooking_method.append(method.text.encode('utf-8'))
 
     save_recipe(ingredients, cooking_method)
 
-#Function to save recipe to database
+#Function to send recipe to email address
 def save_recipe(ingredients, cooking_method):
-    save = recipe.Recipe(ingredients, cooking_method)
+    email_address = "dara.shorten@gmail.com"
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_address, 'poiuytr987')
+
+    #Create message including ingredients and cooking method
+    msg = ""
+    for ingredient in ingredients:
+        msg += ingredient
+    for method in cooking_method:
+        msg += method+"\n"
+    
+    server.sendmail(email_address, email_address, msg)
+    server.quit()
 
 #Function to retrieve recipes at declared time
 def get_daily_picks():
@@ -49,4 +69,5 @@ def get_daily_picks():
                 #TODO Verify url is /food/recipes
                 daily_picks.append(link['href'])
     
-get_daily_picks()
+#get_daily_picks()
+get_recipe("food/recipes/herbycouscouswithbut_92552")
